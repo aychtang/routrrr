53b6d2b9973dd218c441db0afffbfa4ed49686d7
Hack Reactor Students <accounts@hackreactor.com>
2013-03-09 14:50:23 -0800

    Middle of the afternoon commit

--- src/client/routrrr.js
+++ src/client/routrrr.js
@@ -1,6 +1,8 @@
   var map;
   var user;
   var currentMarker;
+  var origin;
+  var myPosition;
 
   var initialise = function() {
 
@@ -10,8 +12,7 @@
 
     //Renders map and puts marker at lat/lon passed into argument.
     var initMap = function(lat, lon){
-      var myPosition = new google.maps.LatLng(lat, lon);
-
+      myPosition = new google.maps.LatLng(lat, lon);
       var mapOptions = {
         center: myPosition,
         zoom: 15,
@@ -19,12 +20,23 @@
       };
 
       map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+      service = new google.maps.places.PlacesService(map);
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        var newLat = event.latLng.lat();
+        var newLon = event.latLng.lng();
+
+        placeMarker(newLat, newLon);
+        drawBounds(newLat, newLon);
+      });
     };
 
     //Sets the latitude and longitude of the user, calls map and marker makers
     var setPosition = function(position){
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
+      myLat = lat;
+      myLon = lon;
       initMap(lat, lon);
       placeMarker(lat, lon);
     };
@@ -40,6 +52,21 @@ if(!Markers){
 
 if (Meteor.isClient) {
 
+  var markerArray = [];
+  var boundArray = [];
+
+  var clearMarkers = function(){
+    for(var i = 0; i < markerArray.length; i++){
+      markerArray[i].setMap(null);
+    }
+  };
+
+  var clearBounds = function(){
+    for(var i = 0; i < boundArray.length; i++){
+      boundArray[i].setMap(null);
+    }
+  };
+
   //places a marker at given lat and lon, inserts position into DB if there is no current marker by user
   var placeMarker = function(lat, lon){
       var position = new google.maps.LatLng(lat, lon);
@@ -48,49 +75,55 @@ if (Meteor.isClient) {
         title: user || 'yo!'
       });
 
-      currentMarker.setMap(map);
-      var isThere = Markers.findOne({user: Meteor.userId()});
+      origin = origin || currentMarker;
 
-      //checks if a marker from the user already exists in DB
-      if(!isThere){
-        Markers.insert({user : Meteor.userId(), position: myPosition});
-      } else {
-        console.log('Marker already logged in DB');
+      if(currentMarker !== origin){
+        markerArray.push(currentMarker);
       }
+      clearMarkers();
+      currentMarker.setMap(map);
     };
 
     //function draws bounding box
-    var drawBounds = function(lat, lon){
+    var drawBounds = function(newLat, newLon){
       var bound;
+
         if(currentMarker){
           var polyCoords =[
-            new google.maps.LatLng(37.774929, -122.419416),
-            new google.maps.LatLng(37.749815, -122.419416),
-            new google.maps.LatLng(37.749815, -122.434902),
-            new google.maps.LatLng(37.774929, -122.434902)
+            new google.maps.LatLng(myLat, newLon),
+            new google.maps.LatLng(myLat, myLon),
+            new google.maps.LatLng(newLat, myLon),
+            new google.maps.LatLng(newLat, newLon),
           ]
+
+          var NE = new google.maps.LatLng(myLat, myLon);
+          var SW = new google.maps.LatLng(newLat, newLon);
+          var boundz = new google.maps.LatLngBounds(SW, NE);
+          var request = {
+            bounds: boundz,
+            types: ['cafe']
+          };
 
           bound = new google.maps.Polygon({
             paths: polyCoords,
             strokeColor: "#333",
             strokeOpacity: 0.8,
-            strokeWeight: 2,
+            strokeWeight: 3,
             fillColor: "rgb(70, 182, 66)",
-            fillOpacity: 0.35
+            fillOpacity: 0.20
           });
-
+          boundArray.push(bound);
+          clearBounds();
           bound.setMap(map);
-      }
-    }
 
-  Template.header.events = {
-      'click .putMarker' : function () {
-        placeMarker(37.749815, -122.434902);
-        drawBounds();
+          service.nearbySearch(request, function(results){
+            console.log(results);
+          });
       }
     };
 
   Meteor.startup(function(){
+
     //Renders map on startup
     Template.map.rendered = function(){
       initialise();
